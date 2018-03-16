b55ed820ababa14f6c6baa9240281b4f25cb520f
Dawod <mhm.dawod1@gmail.com>
2018-03-16 11:05:29 +0200

    data structure for collecting data before pushing it to database

---
+++ src/thunder/Data.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thunder;
+
+/**
+ *
+ * @author dawod
+ */
+public class Data {
+    
+    private String stem;
+    private String link;
+    private String original;
+    private String tag;
+    private int position;
+    
+    public Data(String stem, String link, String original, String tag , int position){
+        this.stem=stem;
+        this.link=link;
+        this.original=original;
+        this.tag=tag;
+        this.position=position;
+        
+    }
+    
+    public String getStem(){
+        return this.stem;
+    }
+    public String getLink(){
+        return this.link;
+    }
+    public String getOriginal(){
+        return this.original;
+    }
+    public String getTag(){
+        return this.tag;
+    }
+    public int getPosition(){
+        return this.position;
+    }
+    
+    
+}
