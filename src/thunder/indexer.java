eb5262661b2857072dab2c2eb324a42784297d6d
Dawod <mhm.dawod1@gmail.com>
2018-03-16 11:04:36 +0200

    modified for new data entry style

--- src/thunder/indexer.java
+++ src/thunder/indexer.java
@@ -12,8 +12,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.tartarus.snowball.ext.englishStemmer;
@@ -23,13 +24,16 @@ import org.tartarus.snowball.ext.englishStemmer;
  * @author dawod
  */
 public class indexer {
+    int counter=0;
     Query q = new Query();
+    List<Data> dataElement = new ArrayList<>();
     String filePath="src/dataset/";
     File directory = new File(filePath);
     File [] files = directory.listFiles();
     BufferedReader br ;
     String line;
     String [] words;
+    List<String> all_words = new ArrayList<String>();
     String small_word,stem_word;
     
     
@@ -37,48 +41,46 @@ public class indexer {
      * needs:
      * I need stooping removing function
      * check that the new word is unique in the database.
-     * 
+     *
      * ____________________________________________________________________
-     * 
+     *
      * this function is responsible for the following
      * 1. i read all files in the directory specified  by the file_path variable
      * 2. for each file i read line by line
      * 3. for each line i separate it's words by spaces (" ")
-     * 4. for each word i convert all letters to lowercase 
+     * 4. for each word i convert all letters to lowercase
      * 5. for each word i do stemming using snowball library , other options : porter(before snowball), lancaster(agressive)
-     * 
-     * 
+     *
+     *
      * finally this function perform some modifications on the database
      * till now i am only save all words i catch even if repeated in the file or in the database!
      */
     public void indexFiles(){
-        
+
         for(int i =0 ; i<files.length ; i++){ //for eatch file in the directory
             if(files[i].isFile()){
                 
                 try {
                     br = new BufferedReader(new FileReader(files[i]));
-                    while((line = br.readLine())!=null){ //for each line in the current file
+                    while((line = br.readLine())!=null ){ //for each line in the current file
                         //System.out.println("file is : ("+files[i]+") line is : ("+line+")");
                         words = line.split(" ");
-                        for(String word : words){
+                        for(String word : words ){
                             // in this section , do what you need for indexing the original word
                             
                             
                             small_word = word.toLowerCase();
                             stem_word = stemmer(small_word);
+                            all_words.add(stem_word);
+                            dataElement.add(new Data(stem_word, "http://link.com", small_word, "header", counter));
+                            
+                            counter++;
+                                    
                             //System.out.println("file : "+files[i]+" ----- original : "+word+" --- stemmed : "+stemmer(word));
                             
-                            q.insert_word(stem_word);
-                        
- 
+                        //    q.insert_all_word(stem_word,all_words);
+
                             
-                            
-                            
-                        
-                        
-                        
-                        
                         }
                     }
                     
@@ -89,26 +91,28 @@ public class indexer {
                 }
             }
             else{
-                System.out.println(files[i]+" is not file!");
+                System.err.println("Dawod : "+files[i]+" is not file!");
             }
             
         }
+        //q.insert_all_word((ArrayList<String>) all_words);
+        q.insert_all_indexes((ArrayList<Data>) dataElement);
     }
     
     
     
-    public void getWords(){
-        SelectResultSet res;
-        res =(SelectResultSet) q.get_words();
-        try {
-            while(res.next()){
-                System.out.println(res.getString("word_val"));
-                
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(indexer.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+//    public void getWords(){
+//        SelectResultSet res;
+//        res =(SelectResultSet) q.get_words();
+//        try {
+//            while(res.next()){
+//                System.out.println(res.getString("word_val"));
+//                
+//            }
+//        } catch (SQLException ex) {
+//            Logger.getLogger(indexer.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
     
     
     
@@ -130,7 +134,7 @@ public class indexer {
         englishStemmer stem = new englishStemmer();
         stem.setCurrent(word);
         if(stem.stem()){
-           // System.out.println(stem.getCurrent()); //comment this line
+            // System.out.println(stem.getCurrent()); //comment this line
             return stem.getCurrent();
         }
         else
