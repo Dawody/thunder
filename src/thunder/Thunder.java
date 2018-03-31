b657e5df2d632f2b03a4102659a32691e9dbf989
Dawod <mhm.dawod1@gmail.com>
2018-03-31 15:38:26 +0200

    multi threaded indexer and update the link status table

--- src/thunder/Thunder.java
+++ src/thunder/Thunder.java
@@ -6,7 +6,11 @@
 package thunder;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -16,20 +20,80 @@ import org.jsoup.nodes.Document;
  */
 public class Thunder {
     
+    
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
         indexer indx = new indexer();
+        Map<Integer,Boolean> linkStatus = new HashMap<Integer,Boolean>();
+        linkStatus = indx.getLinkStatus();
+        Integer counter = indx.initFileCounter();
+        Query qr = indx.initQuery();
+        
+        
 //TEST insertion
 
 
         for(int i=0 ; i<1 ; i++){
-            indx.indexFiles();
-            System.out.println("counter = "+i);
-
+            //indx.indexFiles();
+            Thread t1 = new Thread(new indexer(linkStatus,counter,qr));
+            Thread t2 = new Thread(new indexer(linkStatus,counter,qr));
+            Thread t3 = new Thread(new indexer(linkStatus,counter,qr));
+            Thread t4 = new Thread(new indexer(linkStatus,counter,qr));
+//            Thread t5 = new Thread(new indexer(linkStatus,counter,qr));
+//            Thread t6 = new Thread(new indexer(linkStatus,counter,qr));
+//            Thread t7 = new Thread(new indexer(linkStatus,counter,qr));
+//            Thread t8 = new Thread(new indexer(linkStatus,counter,qr));
+//            Thread t9 = new Thread(new indexer(linkStatus,counter,qr));
+//            Thread t10 = new Thread(new indexer(linkStatus,counter,qr));
+            t1.start();
+            t2.start();
+            t3.start();
+            t4.start();
+//            t5.start();
+//            t6.start();
+//            t7.start();
+//            t8.start();
+//            t9.start();
+//            t10.start();
+            
+            
+            try {
+                t1.join();
+                t2.join();
+                t3.join();
+                t4.join();
+//                t5.join();
+//                t6.join();
+//                t7.join();
+//                t8.join();
+//                t9.join();
+//                t10.join();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Thunder.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
         }
+        
+        System.out.println("indexing finish..");
+//        ArrayList<Integer> linkIds = new ArrayList<Integer>();
+//        for (Map.Entry<Integer, Boolean> entry : linkStatus.entrySet())
+//        {
+//            if(entry.getValue())
+//            {
+//                linkIds.add(entry.getKey());
+//            }
+//                
+//                            
+//        }
+//        System.out.println("start to update link status..");
+//        indx.updateLinkStatus(linkIds);
+        
 
 
 
