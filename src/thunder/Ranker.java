93bec0f86ce0e507a32dee9ec370c66e52284ecf
amir-farag <eng.amirf@gmail.com>
2018-04-26 02:14:51 +0200

    some changes in project

--- src/thunder/Ranker.java
+++ src/thunder/Ranker.java
@@ -1,6 +1,9 @@
 package thunder;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Ranker{
 
@@ -9,16 +12,25 @@ public class Ranker{
     private Popularity p;
     private Relevance r;
 
-    Ranker(String query){
+    Ranker(){
         /*db = new RankerDBConnection();
         individual_score=0.0;*/
         p = new Popularity();
-        r = new Relevance(query);
+        r = new Relevance();
     }
     public void calculateScore(){
     }
-    public static void main(String args[]){
-        Ranker r1 = new Ranker("netherlands vs portugal today");
+    public static void main(String args[]) throws SQLException {
+        Ranker r1 = new Ranker();
+        /*Map<String, Double> mr;
+        String [] s1 = {"netherlands","vs","portugal", "today"};
+        mr = r1.r.tfidf(s1);*/
+        ArrayList<String> s = r1.p.page_ranker();
+        for (int i=0; i<s.size();i++)
+        {
+            System.out.print(s.get(i)+" ");
+        }
+
     }
 
 }
